be72a086ca7dcf52c7385571dff0ee13ef1f10d6
= <=>
2023-02-11 18:32:27 +0530

    added void function in q3

--- Question3.cpp
+++ Question3.cpp
@@ -1,18 +1,27 @@
 #include<iostream>
 using namespace std;
 
+void factors(int number = 10){
+	int counter = 1;
+	while(counter <= number){
+		if(!(number%counter)){
+			cout << counter << ", ";
+		}
+		counter++;
+	}
+}
+
 int main(){
 	int num, counter = 1;
 	
 	cout << "Enter the number: ";
 	cin >> num;
-	cout << endl << "The factors of the number are : " << endl;
 	
-	while(counter <= num){
-		if(!(num%counter)){
-			cout << counter << ", ";
-		}
-		counter++;
+	if(!num){
+		cout << "Number is zero.";
 	}
-	
+	else{
+		cout << endl << "The factors of the number are : " << endl;
+		factors(num);
+	}
 }
