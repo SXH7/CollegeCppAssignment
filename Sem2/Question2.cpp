d4f8495186c0dddf628d5963f87bc24660656266
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2023-04-28 12:52:31 +0530

    Create Question2.cpp

---
+++ Sem2/Question2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    int i = 0;
+    while(i<n){
+        int j = i;
+        int min = i;
+        while(j<n){
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+            j++;
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        i++;
+    }
+    
+    for(int x = 0; x<n; x++){
+        cout << arr[x] << endl;
+    }
+}
+
+int main() {
+    int a[] = {4, 3, 65, 3, 6, 4356};
+    insertionSort(a, 6);
+}
